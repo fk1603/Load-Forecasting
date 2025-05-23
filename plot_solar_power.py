57fdfe37c6ab9d395cdd6d9c38b2dc31cd281ddc
fk1603 <kuehnflorian@gmx.de>
2025-05-23 09:59:02 +0200

    comments

--- plot_solar_power.py
+++ plot_solar_power.py
@@ -1,3 +1,7 @@
+'''
+Creates a plot for the estimated solar power generation between 2015-2024.
+'''
+
 import matplotlib.pyplot as plt
 
 years = list(range(2015, 2025))
@@ -5,14 +9,12 @@ solar_capacity = [2.52, 5.04, 7.56, 10.08, 12.6, 18.6, 29.2, 41.53, 69.55, 85.0]
 
 plt.figure(figsize=(9, 4))
 
-# Plot estimated start (2015–2018)
+# Plot estimates
 plt.plot(years, solar_capacity, marker='o', linestyle='-', color='blue', label='Estimated')
 
 # Plot actual data (2019–2023)
 plt.plot(years[4:9], solar_capacity[4:9], marker='s', linestyle='-', color='orange', label='Actual')
 
-# Plot estimated end (2024), connecting to previous
-#plt.plot(years[8:10], solar_capacity[8:10], marker='s', linestyle='-', color='gray')
 
 # Annotate all points
 for year, capacity in zip(years, solar_capacity):
